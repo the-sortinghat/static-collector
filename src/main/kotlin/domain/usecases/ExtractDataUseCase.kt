375fb4eb2ff48a421035c43af4afb4478e577580
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-10-19 00:24:46 -0300

    add extract data usecase
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ src/main/kotlin/domain/usecases/ExtractDataUseCase.kt
@@ -0,0 +1,32 @@
+package domain.usecases
+
+import domain.conversors.ConverterToPIM
+import domain.entities.platform_independent_model.System
+import domain.fetchers.FetchData
+import domain.parsers.*
+import domain.repositories.SystemRepository
+
+class ExtractDataUseCase(
+    private val fetchData: FetchData,
+    private val parseData: ParseData,
+    private val converterToPim: ConverterToPIM,
+    private val systemRepository: SystemRepository,
+) {
+
+    fun run(url: String): System {
+        val response = fetchData.run(url)
+        val psm = parseData.run(response)
+        val system = converterToPim.run(psm)
+        persist(system)
+        return system
+    }
+
+    private fun persist(system: System) {
+        val sys = systemRepository.findByName(system.name)
+
+        if (sys != null)
+            throw Exception("system with this name already exists")
+
+        systemRepository.save(system)
+    }
+}
