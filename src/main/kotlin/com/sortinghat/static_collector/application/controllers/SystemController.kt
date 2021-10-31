c3b7735582324554fb7e3525e8784a011d372234
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-10-30 23:59:30 -0300

    add system controller and register method
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ src/main/kotlin/com/sortinghat/static_collector/application/controllers/SystemController.kt
@@ -0,0 +1,40 @@
+package com.sortinghat.static_collector.application.controllers
+
+import com.sortinghat.static_collector.application.http.requests.RegisterSystemRequest
+import com.sortinghat.static_collector.application.http.responses.RegisterSystemResponse
+import com.sortinghat.static_collector.application.http.HTTPHandler
+import com.sortinghat.static_collector.application.yaml.YAMLParser
+import com.sortinghat.static_collector.domain.converters.DockerComposeConverter
+import com.sortinghat.static_collector.domain.entities.platform_independent_model.System
+import com.sortinghat.static_collector.domain.fetchers.DockerComposeFetch
+import com.sortinghat.static_collector.domain.ports.repositories.SystemRepository
+import com.sortinghat.static_collector.domain.usecases.ExtractDataUseCase
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/systems")
+class SystemController {
+
+    @PostMapping("/register")
+    fun registerSystem(@RequestBody request: RegisterSystemRequest): RegisterSystemResponse {
+        val extractDataUseCase = ExtractDataUseCase(
+            DockerComposeFetch(HTTPHandler()),
+            YAMLParser(),
+            DockerComposeConverter(),
+            MockSystemRepo()
+        )
+
+        val system = extractDataUseCase.run(request.repoUrl)
+        return RegisterSystemResponse(system)
+    }
+
+    inner class MockSystemRepo : SystemRepository {
+        override fun save(system: System) {
+            println("System saved")
+        }
+
+        override fun findByName(name: String): System? {
+            return null
+        }
+    }
+}
---
+++ src/main/kotlin/com/sortinghat/static_collector/application/http/requests/RegisterSystemRequest.kt
@@ -0,0 +1,3 @@
+package com.sortinghat.static_collector.application.http.requests
+
+data class RegisterSystemRequest(val repoUrl: String)
