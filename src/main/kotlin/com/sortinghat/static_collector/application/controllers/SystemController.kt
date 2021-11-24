9a5e24448f5d1174fddab01457cb54c0f7cbd09d
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-11-24 03:13:00 -0300

    refactor POST /systems tests
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- src/main/kotlin/com/sortinghat/static_collector/application/controllers/SystemController.kt
+++ src/main/kotlin/com/sortinghat/static_collector/application/controllers/SystemController.kt
@@ -3,6 +3,10 @@ package com.sortinghat.static_collector.application.controllers
 import com.sortinghat.static_collector.application.factories.ExtractDataFactory
 import com.sortinghat.static_collector.application.http.requests.RegisterSystemRequest
 import com.sortinghat.static_collector.application.http.responses.RegisterSystemResponse
+import com.sortinghat.static_collector.domain.exceptions.UnableToFetchDataException
+import com.sortinghat.static_collector.domain.exceptions.UnableToParseDataException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -10,9 +14,21 @@ import org.springframework.web.bind.annotation.*
 class SystemController(private val extractDataFactory: ExtractDataFactory) {
 
     @PostMapping
-    fun registerSystem(@RequestBody request: RegisterSystemRequest): RegisterSystemResponse {
-        val extractDataUseCase = extractDataFactory.create()
-        val system = extractDataUseCase.run(request.repoUrl)
-        return RegisterSystemResponse(system)
+    fun registerSystem(@RequestBody request: RegisterSystemRequest): ResponseEntity<RegisterSystemResponse> {
+        return try {
+            val extractDataUseCase = extractDataFactory.create()
+            val system = extractDataUseCase.run(request.repoUrl)
+            ResponseEntity(RegisterSystemResponse(system), HttpStatus.OK)
+        } catch (e: Exception) {
+            handleExceptions(e)
+        }
+    }
+
+    private fun <T> handleExceptions(e: Exception): ResponseEntity<T> {
+        return when (e) {
+            is UnableToFetchDataException -> ResponseEntity<T>(null, HttpStatus.NOT_FOUND)
+            is UnableToParseDataException -> ResponseEntity<T>(null, HttpStatus.BAD_REQUEST)
+            else -> ResponseEntity<T>(null, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 }
